265e06d201072bebcb103b8eac254799a1c1e8ea
Mohd Rejoan <wearecrx@gmail.com>
2023-06-10 12:45:40 +0600

    plan your best trip section complete

--- landing-page-rejoan.php
+++ landing-page-rejoan.php
@@ -14,7 +14,7 @@
   <link rel="stylesheet" href="./css/style.css" />
 </head>
 
-<body class=" bg-secondary ">
+<body>
 
 
   <!-- main section wrapper -->
@@ -120,7 +120,93 @@
       </div>
     </div>
   </main>
+  <!-- plan your best trip eveer  -->
+  <section class=" my-100 ">
+    <div class="container">
+      <div class="row">
+        <div class="col-8">
+          <h1>Plan your best Trip ever</h1>
+          <p>Making the Most of Your Travel Experience in 2023</p>
+        </div>
+        <div class="col-4 text-end ">
+          <a href="" class=" btn-primary-outline ">view all destination</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <div>
+            <div class="card border border-0">
+              <img src="./images/Frame 17.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Morocco</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div>
+            <div class="card border border-0">
+              <img src="./images/Frame 19.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Mexico</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div>
+            <div class="card border border-0">
+              <img src="./images/Frame 17.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Croatia</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1>Top Location to Explore</h1>
+      <p>Here are some of the most visited places in 2023<a href="#" class="previous round">&#8249;</a>
+        <a href="#" class="next round">&#8250;</a>
+      </p>
+      <!-- <i class="bi bi-arrow-right"></i> -->
+      <div class="row">
+        <div class="col-4">
+          <div class="card border border-0">
+            <div class="card">
+              <img class="card-img-top" src="./images/image 10.png" alt="">
+              <div class="card-body">
+                <p class="card-text">Tokyo,Japan</p>
+                <h5 class="card-title">The Shibuya</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card border border-0">
+            <div class="card">
+              <img class="card-img-top" src="./images/image 11.png" alt="">
+              <div class="card-body">
+                <p class="card-text">Rome, Italy</p>
+                <h5 class="card-title">The Colosseum</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card border border-0">
+            <div class="card">
+              <img class="card-img-top" src="./images/image 10.png" alt="">
+              <div class="card-body">
+                <p class="card-text">Capetown ,South Africa</p>
+                <h5 class="card-title">The Blyde River Canyon</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+  </section>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
   </script>
