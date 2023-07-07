644cbb5a028e396eff6f321dcbc2f39ec7a5966e
Shourav <shourav8989@gmail.com>
2023-07-07 20:10:35 +0600

    all post page added

--- template/admin/all-post.php
+++ template/admin/all-post.php
@@ -21,24 +21,58 @@
   <div id="app">
     <!-- sidebar  -->
     <?php include('./partials/SideBar.php') ?>
-    <!-- main content section  -->
+    <!-- main section -->
     <div id="main">
       <header class="mb-3">
         <a href="#" class="burger-btn d-block d-xl-none">
           <i class="bi bi-justify fs-3"></i>
         </a>
       </header>
-      <div class="page-heading">
-        <h3>All Post Table</h3>
-      </div>
-      <section class="section ">
-        <div class="card">
-          <div class="card-body">
-
+      <!-- Writting Settings start -->
+      <div class="row">
+        <div class="col-12">
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <table class=" table table-md w-100 border ">
+                <thead>
+                  <tr>
+                    <th class=" col-1 ">
+                      <input type="checkbox" class=" form-check-input "> <span class=" ms-2 ">All</span>
+                    </th>
+                    <th class=" col-8 ">Post</th>
+                    <th class=" col-3 ">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
+                    <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                    <td class="col-3 ">
+                      <button class=" btn btn-sm btn-outline-primary ">Edit</button>
+                      <button class=" btn btn-sm btn-outline-success ">View</button>
+                      <button class=" btn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
+                    <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                    <td class="col-3 ">
+                      <button class=" btn btn-sm btn-outline-primary ">Edit</button>
+                      <button class=" btn btn-sm btn-outline-success ">View</button>
+                      <button class=" btn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-      </section>
+      </div>
+
     </div>
+    <!-- Writting Settings End -->  
   </div>
   <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
   <script src="assets/js/bootstrap.bundle.min.js"></script>
