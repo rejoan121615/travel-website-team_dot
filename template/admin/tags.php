10376d15d2d9439a59baa4403928207a685527c7
Shourav <shourav8989@gmail.com>
2023-07-07 20:16:50 +0600

    Tags page added

--- template/admin/tags.php
+++ template/admin/tags.php
@@ -26,16 +26,68 @@
           <i class="bi bi-justify fs-3"></i>
         </a>
       </header>
-      <div class="page-heading">
-        <h3>Tags</h3>
-      </div>
-      <section class="section ">
-        <div class="card">
-          <div class="card-body">
 
+<div class="page-heading">
+  <h3>Tags</h3>
+</div>
+<!-- Writting Settings start -->
+<div class="row">
+  <div class="col-12">
+    <div class="card">
+      <div class="card-content">
+        <div class="card-body">
+          <h4 class="card-title">Add new Tags</h4>
+
+          <div class="row">
+            <div class="col-12"> <input type="text" class="form-control is-valid" id="valid-state"
+                placeholder="Valid" value="Valid" required>
+              <div class="valid-feedback">
+                <i class="bx bx-radio-circle"></i>
+              </div>
+              <div class=" mt-3"><input class="btn btn-primary" type="submit" value="Submit"></div>
+            </div>
           </div>
         </div>
-      </section>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <table class=" table table-md w-100 border ">
+          <thead>
+            <tr>
+              <th class=" col-1 ">
+                <input type="checkbox" class=" form-check-input "> <span class=" ms-2 ">All</span>
+              </th>
+              <th class=" col-8 ">Tags</th>
+              <th class=" col-3 ">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
+              <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+              <td class="col-3 ">
+                <button class=" btn btn-sm btn-outline-primary ">Edit</button>
+                <button class=" btn btn-sm btn-outline-danger">Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
+              <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+              <td class="col-3 ">
+                <button class=" btn btn-sm btn-outline-primary ">Edit</button>
+                <button class=" btn btn-sm btn-outline-danger">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+<!-- Writting Settings End -->
     </div>
   </div>
   <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
