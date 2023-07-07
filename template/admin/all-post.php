e19fe7ba508717859e30367915f90928a539d8e0
Mohd Rejoan <wearecrx@gmail.com>
2023-07-07 20:51:54 +0600

    create new post button created

--- template/admin/all-post.php
+++ template/admin/all-post.php
@@ -34,65 +34,55 @@
       <!-- Writting Settings start -->
       <div class="row">
         <div class="col-12">
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-content">
-              <div class="card-body">
-                <h4 class="card-title">Add New Post</h4>
-
-                <div class="row">
-                  <div class="col-12"> <input type="text" class="form-control is-valid" id="valid-state"
-                      placeholder="Valid" value="Valid" required>
-                    <div class="valid-feedback">
-                      <i class="bx bx-radio-circle"></i>
-                    </div>
-                    <div class=" mt-3"><input class="btn btn-primary" type="submit" value="Submit"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <table class=" table table-md w-100 border ">
-                <thead>
-                  <tr>
-                    <th class=" col-1 ">
-                      <input type="checkbox" class=" form-check-input "> <span class=" ms-2 ">All</span>
-                    </th>
-                    <th class=" col-8 ">Post</th>
-                    <th class=" col-3 ">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
-                    <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
-                    <td class="col-3 ">
-                      <button class=" btn btn-sm btn-outline-primary ">Edit</button>
-                      <button class=" btn btn-sm btn-outline-success ">View</button>
-                      <button class=" btn btn-sm btn-outline-danger">Delete</button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
-                    <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
-                    <td class="col-3 ">
-                      <button class=" btn btn-sm btn-outline-primary ">Edit</button>
-                      <button class=" btn btn-sm btn-outline-success ">View</button>
-                      <button class=" btn btn-sm btn-outline-danger">Delete</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-content">
+          <div class="card-body">
+            <a href="new-post.php" class=" d-block btn btn-primary fw-bold">Create new post</a>
           </div>
         </div>
       </div>
-
+      <div class="card">
+        <div class="card-body">
+          <table class=" table table-md w-100 border ">
+            <thead>
+              <tr>
+                <th class=" col-1 ">
+                  <input type="checkbox" class=" form-check-input "> <span class=" ms-2 ">All</span>
+                </th>
+                <th class=" col-8 ">Post</th>
+                <th class=" col-3 ">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
+                <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                <td class="col-3 ">
+                  <button class=" btn btn-sm btn-outline-primary ">Edit</button>
+                  <button class=" btn btn-sm btn-outline-success ">View</button>
+                  <button class=" btn btn-sm btn-outline-danger">Delete</button>
+                </td>
+              </tr>
+              <tr>
+                <td class="col-1 "><input type="checkbox" class=" form-check-input "></td>
+                <td class="col-8 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                <td class="col-3 ">
+                  <button class=" btn btn-sm btn-outline-primary ">Edit</button>
+                  <button class=" btn btn-sm btn-outline-success ">View</button>
+                  <button class=" btn btn-sm btn-outline-danger">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-    <!-- Writting Settings End -->  
+  </div>
+
+  </div>
+  <!-- Writting Settings End -->
   </div>
   <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
   <script src="assets/js/bootstrap.bundle.min.js"></script>
